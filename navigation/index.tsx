b880ecc325136e346685bb1c0f2b44db901e051f
jeiDev <joseanyeloserrano@gmail.com>
2021-11-27 19:10:20 -0400

    create welcome screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,23 +1,21 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { ColorSchemeName, Text } from 'react-native';
+import { ColorSchemeName } from 'react-native';
 
-import ModalScreen from '../screens/ModalScreen';
 import { RootStackParamList } from '../types';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import ModalScreen from '../screens/ModalScreen';
+import WelcomeScreen from '../screens/Welcome';
+
+const Stack = createNativeStackNavigator();
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
     <NavigationContainer 
       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack.Navigator initialRouteName="Modal">
-        {/* <Stack.Screen name="Root" component={() => {
-          return (
-            <Text>Hello world</Text>
-          )
-        }} options={{ headerShown: false }} /> */}
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" options={{header: () => (null)}} component={WelcomeScreen} />
         <Stack.Group screenOptions={{ presentation: 'modal' }}>
           <Stack.Screen name="Modal" component={ModalScreen} />
         </Stack.Group>
---
+++ settings/theme.setting.ts
@@ -0,0 +1,3 @@
+export const themeSetting = {
+    color: "#6B56AB"
+}
